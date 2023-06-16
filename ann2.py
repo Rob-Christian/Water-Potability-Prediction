c4aedf6eb89c9a7bbb4eaf0076163c2fd9cc215e
Rob-Christian <59250293+Rob-Christian@users.noreply.github.com>
2023-06-16 17:14:21 +0800

    Update ann2.py

--- ann2.py
+++ ann2.py
@@ -70,7 +70,7 @@ for lc in l:
         NN2_test_NPV.append(tn/(tn + fn))
         
         # Saves model architecture
-        model.save(r'C:\Users\rmcad\Downloads\Research\Water Potability\Architecture1\a{}.h5'.format(i_s+1))
+        model.save(r'C:\Users\...\Architecture1\a{}.h5'.format(i_s+1))
         i_s += 1
     
     # Updates the mean metrics for each fold
